e8be888510a00ae8c9a4ac645c055e09a79e972c
Janine-thon <janine@ba.estudante.senai.br>
2025-11-04 10:38:09 -0300

    Add files via upload

---
+++ aula17/src/pages/SobreNosPage/index.jsx
@@ -0,0 +1,21 @@
+//src\pages\SobreNosPage\index.jsx//
+
+import './style.css'
+
+export default function SobreNosPage(){
+         return(
+        <section className="sobre"> 
+            <h1>Sobre Nós</h1>
+            <p>
+                Somos uma empresa de contabilidade com foco em pequenas e médias empresas.
+                Acreditamod que finanças claras e processos simples aceleram o crescimento.
+            </p>
+            <ul>
+                <li> +10 anos de empresa </li>
+                <li> Equipe Sênior Atendimento </li>
+                <li> Parcerias com ERPs e bancos digitais </li>
+
+            </ul>
+        </section>
+    )
+}
